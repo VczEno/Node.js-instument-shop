d35d0211b3dd878f47de9caf01431b128b6a491f
VczEno <arpaia.vin@gmail.com>
2024-05-18 16:53:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require ('fs')
+const http = require ('http')
+const url = require ('url')
+
+
+//SERVER 
+const server=http.createServer((req,res) => {
+
+    res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening request on port 8000')
+})
